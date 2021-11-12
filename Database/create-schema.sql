0ec127f58b059c465e52171781434d35a76f694a
Théo Roncalli <theo.roncalli@yahoo.fr>
2021-11-12 19:34:53 +0100

    Enhancements of the performances for the database

--- Database/create-schema.sql
+++ Database/create-schema.sql
@@ -1,7 +1,3 @@
--- DROP DATABASE IF EXISTS GenesAnnotation;
--- CREATE DATABASE GenesAnnotation;
-
-DROP SCHEMA IF EXISTS GenesAnnotation CASCADE;
 -- The authorization scheme is a good idea once the site
 -- will be operational and deployed.
 -- For debug and development, we will keep it commented out.
@@ -12,9 +8,9 @@ CREATE DOMAIN phone_number AS
     VARCHAR(16)
     CONSTRAINT check_E164_format CHECK (VALUE ~ '^\+\d{9,15}');
 
-CREATE DOMAIN gene_name AS
+CREATE DOMAIN AC AS
     CHAR(8) NOT NULL
-    CONSTRAINT check_norm_gene_name CHECK (VALUE ~ '^[A-Z]{3}\d{5}');
+    CONSTRAINT check_norm_accession_number CHECK (VALUE ~ '^[A-Z]{3}\d{5}');
 
 CREATE DOMAIN gene_sequence AS
     VARCHAR(6000) NOT NULL
@@ -34,59 +30,58 @@ CREATE TABLE IF NOT EXISTS member(
     PRIMARY KEY (email),
     CONSTRAINT check_role_available CHECK (role IN ('reader','annotator','validator')));
 
+CREATE TABLE IF NOT EXISTS genome(
+    chromosome VARCHAR(20),
+    specie VARCHAR(20),
+    strain VARCHAR(10),
+    sequence TEXT,
+    length INTEGER,
+    PRIMARY KEY (chromosome));
+
+CREATE TABLE IF NOT EXISTS gene_protein(
+    accession_number AC,
+    dna_length INTEGER,
+    start_position INTEGER,
+    end_position INTEGER,
+    reading_frame INTEGER,
+    transcript_biotype VARCHAR(30),
+    chromosome VARCHAR(20),
+    isAnnotated BOOLEAN,
+    PRIMARY KEY (accession_number),
+    CONSTRAINT fkey_gene_protein_chromosome FOREIGN KEY (chromosome) REFERENCES genome(chromosome));
+
+CREATE TABLE IF NOT EXISTS gene_seq(
+    accession_number AC,
+    sequence gene_sequence,
+    PRIMARY KEY (accession_number),
+    CONSTRAINT fkey_gene_ac FOREIGN KEY (accession_number) REFERENCES gene_protein(accession_number));
+
+CREATE TABLE IF NOT EXISTS protein_seq(
+    accession_number AC,
+    sequence protein_sequence,
+    PRIMARY KEY (accession_number),
+    CONSTRAINT fkey_protein_ac FOREIGN KEY (accession_number) REFERENCES gene_protein(accession_number));
+
+CREATE TABLE IF NOT EXISTS history_annot(
+    id SERIAL UNIQUE,
+    isAccepted BOOLEAN,
+    comment VARCHAR(500),
+    time TIMESTAMP,
+    PRIMARY KEY (id, time));
+
 CREATE TABLE IF NOT EXISTS annotation(
-    id SERIAL,
+    accession_number AC,
+    name CHAR(5),
     gene_symbol VARCHAR(10),
     gene_biotype VARCHAR(30),
     transcript_biotype VARCHAR(30),
     function VARCHAR(50),
     status VARCHAR(10),
-    PRIMARY KEY (id),
-    CONSTRAINT check_status_available CHECK (status IN ('ongoing','approved','rejected')));
-
-CREATE TABLE IF NOT EXISTS genome(
-    specie VARCHAR(20),
-    strain VARCHAR(10),
-    chromosome VARCHAR(20) CONSTRAINT Unique_chromosome UNIQUE,
-    sequence TEXT,
-    length INTEGER,
-    PRIMARY KEY (specie,strain));
-
-CREATE INDEX index_strain ON genome USING hash (strain);
-
-CREATE TABLE IF NOT EXISTS gene_protein(
-    name gene_name,
-    gene_symbol VARCHAR(10),
-    gene_biotype VARCHAR(30),
-    length_dna INTEGER,
-    start_position INTEGER,
-    end_position INTEGER,
-    reading_direction INTEGER,
-    transcript_biotype VARCHAR(30),
-    function VARCHAR(50),
-    length_aa INTEGER,
-    specie VARCHAR(20),
-    strain VARCHAR(10),
-    PRIMARY KEY (name),
-    CONSTRAINT fkey_specie_strain FOREIGN KEY (specie,strain) REFERENCES genome(specie,strain));
-
-CREATE TABLE IF NOT EXISTS gene_seq(
-    name gene_name,
-    sequence gene_sequence,
-    PRIMARY KEY (name),
-    CONSTRAINT fkey_gene_name FOREIGN KEY (name) REFERENCES gene_protein(name));
-
-CREATE TABLE IF NOT EXISTS protein_seq(
-    name gene_name,
-    sequence protein_sequence,
-    PRIMARY KEY (name),
-    CONSTRAINT fkey_protein_name FOREIGN KEY (name) REFERENCES gene_protein(name));
-
-CREATE TABLE IF NOT EXISTS annot(
     email VARCHAR(50),
     id BIGINT,
-    name gene_name,
-    PRIMARY KEY (email,id,name),
-    FOREIGN KEY (email) REFERENCES member(email),
-    FOREIGN KEY (id) REFERENCES annotation(id),
-    FOREIGN KEY (name) REFERENCES gene_protein(name));
+    PRIMARY KEY (accession_number),
+    CONSTRAINT fkey_annotation_email FOREIGN KEY (email) REFERENCES member(email),
+    CONSTRAINT check_status_available CHECK (status IN ('ongoing','approved','rejected')),
+    CONSTRAINT fkey_annotation_id FOREIGN KEY (id) REFERENCES history_annot(id));
+
+CREATE INDEX index_strain ON genome USING hash (strain);
--- Database/drop-schema.sql
+++ Database/drop-schema.sql
@@ -1,5 +1,5 @@
 SET search_path TO GenesAnnotation;
 
-DROP TABLE IF EXISTS annot, protein_seq, gene_seq, gene_protein, genome, annotation, member;
-DROP DOMAIN IF EXISTS phone_number, gene_name, gene_sequence, protein_sequence;
-DROP SCHEMA GenesAnnotation;
+DROP TABLE IF EXISTS annotation, history_annot, protein_seq, gene_seq, gene_protein, genome, member;
+DROP DOMAIN IF EXISTS PHONE_NUMBER, AC, GENE_SEQUENCE, PROTEIN_SEQUENCE;
+DROP SCHEMA IF EXISTS GenesAnnotation;
