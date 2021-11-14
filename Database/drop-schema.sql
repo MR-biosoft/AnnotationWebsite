3799dc43976ab497c308844ba038958b9974b629
Théo Roncalli <theo.roncalli@yahoo.fr>
2021-11-14 11:45:33 +0100

    Updating file for dropping database

--- Database/drop-schema.sql
+++ Database/drop-schema.sql
@@ -1,5 +1,5 @@
 SET search_path TO GenesAnnotation;
 
-DROP TABLE IF EXISTS annotation, history_annot, protein_seq, gene_seq, gene_protein, genome, member;
-DROP DOMAIN IF EXISTS PHONE_NUMBER, AC, GENE_SEQUENCE, PROTEIN_SEQUENCE;
+DROP TABLE IF EXISTS decision, annotation, protein_seq, gene_seq, gene_protein, genome, member;
+DROP DOMAIN IF EXISTS PHONE_NUMBER, AC, GENOME_SEQUENCE, GENE_SEQUENCE, PROTEIN_SEQUENCE;
 DROP SCHEMA IF EXISTS GenesAnnotation;
