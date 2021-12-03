60fea6a0105a1fe673254610692fdf96d029f01e
Theo-Roncalli <theo.roncalli@yahoo.fr>
2021-12-03 01:26:08 +0100

    :construction: adding regexes for protein and genome
    .

--- Python/prokaryote/annotation/bioregex.py
+++ Python/prokaryote/annotation/bioregex.py
@@ -14,3 +14,18 @@ DEFAULT_CDS = {
     "gene_symbol": r"(?P<gene_symbol>(?<=gene\_symbol:)\w+)",
     "function": r"(?P<function>(?<=description:)[\w|\d|\s]+)",
 }
+
+DEFAULT_PROTEIN = {
+    "chromosome": r"(?P<chromosome>(?<=chromosome:)[\d|\D]*(?=:Chromosome))",
+    "start_end": r"(?P<start_end>(?<=Chromosome:)\d+:\d+(?=:[1|\-1]))",
+    "gene_name": r"(?P<gene_name>(?<=gene:)[\d|\D]*(?=\s+gene_biotype))",
+    "gene_biotype": r"(?P<gene_biotype>(?<=gene\_biotype:)\w+)",
+    "transcript_biotype": r"(?P<transcript_biotype>(?<=transcript\_biotype:)\w+)",
+    "gene_symbol": r"(?P<gene_symbol>(?<=gene\_symbol:)\w+)",
+    "function": r"(?P<function>(?<=description:)[\w|\d|\s]+)",
+}
+
+DEFAULT_GENOME = {
+    "chromosome": r"(?P<chromosome>(?<=chromosome:)[\d|\D]*(?=:Chromosome))",
+    "start_end": r"(?P<start_end>(?<=Chromosome:)\d+:\d+(?=:[1|\-1]))",
+}
