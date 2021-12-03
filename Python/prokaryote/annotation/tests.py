1afc2b184120f70ed548804aab6b05c0ef878d4a
Theo-Roncalli <theo.roncalli@yahoo.fr>
2021-12-03 01:27:34 +0100

    :white_check_mark: update unit tests for annotation
    regex parsing tests for genome and protein

--- Python/prokaryote/annotation/tests.py
+++ Python/prokaryote/annotation/tests.py
@@ -22,6 +22,11 @@ class FASTAparserTest(TestCase):
         # via github actions. When running the tests on a local machine,
         # export the variable to be the project's root as follows:
         # $ GITHUB_WORKSPACE='/path/to/project/root' python manage.py test
+        # Create three parsers
+        cls.gene_parser = FASTAParser(DEFAULT_CDS)
+        cls.protein_parser = FASTAParser(DEFAULT_PROTEIN)
+        cls.genome_parser = FASTAParser(DEFAULT_GENOME)
+        # Create auxiliary data locations and file lists
         cls._data_dir = Path(get_env_value("GITHUB_WORKSPACE")).joinpath("Data")
         cls._cds_files = list(cls._data_dir.glob("*cds.fa"))
         cls._protein_files = list(cls._data_dir.glob("*pep.fa"))
@@ -29,25 +34,35 @@ class FASTAparserTest(TestCase):
         for i in cls._cds_files + cls._protein_files:
             _genomes.remove(i)
         cls._genome_files = _genomes
+        # Create single test sequences (first objects)
         cls.gene = next(SeqIO.parse(cls._cds_files[0], "fasta"))
         cls.protein = next(SeqIO.parse(cls._protein_files[0], "fasta"))
         cls.genome = next(SeqIO.parse(cls._genome_files[0], "fasta"))
 
     def test_gene_parsing_return_type(self):
-        gene_parser = FASTAParser(DEFAULT_CDS)
-        parsed_dict = gene_parser(self.gene)
+        """Test that the parser effectively returns a dictionary"""
+        parsed_dict = self.gene_parser(self.gene)
         self.assertIsInstance(parsed_dict, dict)
 
     def test_gene_parsing_regex_matches_one(self):
-        gene_parser = FASTAParser(DEFAULT_CDS)
-        parsed_dict = gene_parser(self.gene)
+        """Test gene regex matches all desired fields"""
+        parsed_dict = self.gene_parser(self.gene)
         _expected = DEFAULT_CDS.keys()
         _observed = parsed_dict.keys()
         self.assertSequenceEqual(_observed, _expected)
 
-    # def test_protein_parsing_regex_matches_one(self):
-    #    gene_parser = FASTAParser(DEFAULT_PROTEIN)
-    #    parsed_dict = gene_parser(self.protein)
-    #    _expected = DEFAULT_PROTEIN.keys()
-    #    _observed = parsed_dict.keys()
-    #    self.assertEquals(set(_observed), set(_expected))
+    def test_protein_parsing_regex_matches_one(self):
+        """Test protein regex matches all desired fields"""
+        parsed_dict = self.protein_parser(self.protein)
+        _expected = DEFAULT_PROTEIN.keys()
+        _observed = parsed_dict.keys()
+        self.assertSequenceEqual(_observed, _expected)
+
+    def test_genome_parsing_regex_matches_one(self):
+        """Test genome regex matches all desired fields"""
+        parsed_dict = self.genome_parser(self.genome)
+        _expected = DEFAULT_GENOME.keys()
+        _observed = parsed_dict.keys()
+        self.assertSequenceEqual(_observed, _expected)
+
+    # def test_genome_description(self)
