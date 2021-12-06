cbe8284d5804727827ee2210715a3b27e10df081
Théo Roncalli <theo.roncalli@yahoo.fr>
2021-12-06 18:13:42 +0100

    :bug: Introduce bug

--- Python/prokaryote/prokaryote/urls.py
+++ Python/prokaryote/prokaryote/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("annotation/", include("annotation.urls")),
+    # path("annotation/", include("annotation.urls")),
+    path("annotation/", include(('annotation.urls', 'annotation'), namespace='annotation')),
 ]
