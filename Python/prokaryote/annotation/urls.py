e948f167ce76c436c22a6296c111291efe721f68
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2021-12-07 18:24:08 +0100

    :bug: fixed django reverse url matching
    .

--- Python/prokaryote/annotation/urls.py
+++ Python/prokaryote/annotation/urls.py
@@ -6,7 +6,7 @@ from annotation import views
 urlpatterns = [
     # path("", views),
     # if we have a class-based view, we have to call  the .as_view() method
-    path("genome", views.GenomeView.as_view()),
+    path("genome", views.GenomeView.as_view(), name="genome"),
     # if we have a function-based view, we simply provide the name
     path("profile", views.something),
 ]
--- Python/prokaryote/prokaryote/urls.py
+++ Python/prokaryote/prokaryote/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("annotation/", include("annotation.urls")),
-    path("annotation/", include(('annotation.urls', 'annotation'), namespace='annotation')),
+    path(
+        "annotation/",
+        include(("annotation.urls", "annotation"), namespace="annotation"),
+    ),
 ]
